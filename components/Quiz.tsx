67d54908c3bb3d6c2e42b77072342a76e8773bbf
Nico Miebach <nico@miebach.one>
2021-04-20 11:45:16 +0200

    updates

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -1,20 +1,67 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function Quiz({ questions }) {
   const [index, setIndex] = useState(0);
+  const [points, setPoints] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+  const [showEndResults, setShowEndResults] = useState(false);
 
-  useEffect(() => {
-    questions.map((question) => {});
-  }, []);
+  const checkQuestion = (answer) => {
+    setShowResults(true);
+    setTimeout(() => {
+      setShowResults(false);
+      if (index === questions.length - 1) {
+        setShowEndResults(true);
+      } else {
+        setIndex(index + 1);
+      }
+    }, 3000);
+    if (answer === questions[index].correct_answer) {
+      setPoints(points + 1);
+    }
+  };
 
   return (
     <div className="max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto mt-16">
-      <h1 className="text-center text-2xl mb-8">{questions[index].question}</h1>
-      <div className="grid grid-cols-2 gap-8">
-        <button className="col-span-1 p-4 rounded bg-white shadow text-center font-semibold text-2xl transition hover:bg-lightBlue-100">
-          {questions[index].correct_answer}
-        </button>
-      </div>
+      {!showEndResults ? (
+        <>
+          <h1 className="text-center text-2xl mb-8">{questions[index].question}</h1>
+          <div className="grid grid-cols-2 gap-8">
+            {questions[index].incorrect_answers.map((answer, i) => (
+              <button
+                key={i}
+                className={`p-4 bg-white rounded shadow transition font-semibold text-2xl ${
+                  showResults && (answer === questions[index].correct_answer ? 'bg-green-500 text-white' : 'bg-red-500 text-white')
+                }`}
+                onClick={() => checkQuestion(answer)}
+                disabled={showResults}
+              >
+                {answer.toString()}
+              </button>
+            ))}
+            <button
+              className={`p-4 bg-white rounded shadow transition font-semibold text-2xl ${
+                showResults &&
+                (questions[index].correct_answer === questions[index].correct_answer ? 'bg-green-500 text-white' : 'bg-red-500 text-white')
+              }`}
+              onClick={() => checkQuestion(questions[index].correct_answer)}
+              disabled={showResults}
+            >
+              {questions[index].correct_answer.toString()}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div>
+          <h1 className="text-4xl text-center mb-8">Deine Ergebnisse</h1>
+          <p>
+            Richtig beantwortete Fragen: <strong>{points}</strong>
+          </p>
+          <p>
+            Falsch beantwortete Fragen: <strong>{questions.length - points}</strong>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Axios from 'axios';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Quiz from '../components/Quiz';
+import { useState } from 'react';
 
 export async function getServerSideProps(context) {
   const res = await Axios.get('https://opentdb.com/api.php?amount=10');
@@ -14,6 +16,8 @@ export async function getServerSideProps(context) {
 }
 
 export default function Home({ questions }) {
+  const [playing, setPlaying] = useState(false);
+
   return (
     <>
       <Head>
@@ -23,9 +27,33 @@ export default function Home({ questions }) {
       <Header />
 
       <main className="min-h-screen bg-blueGray-100 px-4 py-16 text-blueGray-700">
-        <div className="max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto mt-16 bg-white rounded shadow p-4">
-          <h1 className="text-3xl text-center font-medium border-b pb-4 border-blueGray-300">Wilkommen bei Quizmania</h1>
-        </div>
+        {playing ? (
+          <Quiz questions={questions} />
+        ) : (
+          <div className="max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto mt-16 bg-white rounded shadow p-4">
+            <h1 className="text-3xl text-center font-medium border-b pb-4 border-blueGray-300 mb-4">Wilkommen bei Quizmania</h1>
+            <div className="grid grid-cols-4 gap-8">
+              <div className="col-span-4 lg:col-span-1">
+                <h3 className="text-2xl">User</h3>
+              </div>
+              <div className="col-span-4 lg:col-span-2 text-center">
+                <h3 className="text-2xl">The Game</h3>
+                <p className="text-blueGray-500 my-4">
+                  Press Play to start the Game as <strong>Guest</strong>
+                </p>
+                <button
+                  className="px-4 py-2 text-xl bg-lightBlue-500 text-white font-semibold uppercase tracking-wider rounded shadow w-full transition hover:bg-lightBlue-600"
+                  onClick={() => setPlaying(true)}
+                >
+                  Play
+                </button>
+              </div>
+              <div className="col-span-4 lg:col-span-1">
+                <h3 className="text-2xl">Highscores</h3>
+              </div>
+            </div>
+          </div>
+        )}
       </main>
 
       <Footer />
